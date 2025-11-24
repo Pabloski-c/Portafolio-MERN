9701c9475f11fd653b405a0c5badafaccc4458b7
Pablo Agustin Torres Lell <lmgb.torreslell.pablo@gmail.com>
2025-11-24 09:07:25 -0300

    Se actualiza API URL para produccion

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -22,7 +22,7 @@ const Contact = () => {
     setStatus('sending');
 
     try {
-      const response = await fetch('http://localhost:5000/api/contact', {
+      const response = await fetch('https://portafolio-mern-api.onrender.com/api/contact', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
